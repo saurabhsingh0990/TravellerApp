2ac67c1a4b2773713a6fea39cdd46240fd87d174
Saurabh Singh <saurabhsingh0990@gmail.com>
2021-11-23 19:49:10 +0530

    This is the front end of the android app. ---------------------------------------------- //1st commit For now there is one bug i.e we cannot fetch 5 day weather forecast because of api limitation rest the frontend is working just fine -------------------------------------------------------- //2nd + 3rd commit fetching forecast data for two days only (as allowed by api) some changes in UI experimented successfully on mapActivity -------------------------------------- //4th commit
    search view added
    -------------------------------------------
    //5th commit
    added navigation drawer
    
    a lot of bugs !
    -------------------------------------------
    //6th commit
    removed navigation drawer
    
    //7th commit
    db connected

--- app/src/main/java/com/example/traveller/MainActivity.java
+++ app/src/main/java/com/example/traveller/MainActivity.java
@@ -7,12 +7,15 @@ import androidx.core.app.ActivityCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Location;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -33,25 +36,76 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 public class MainActivity extends AppCompatActivity {
-    Button login;
+    Button login, signup;
     LinearLayout page;
+    EditText name, eid, pnumber, password, username, userpassword;
+    userDBhelper DB;
+    public void getElements(){
+        page = findViewById(R.id.page);
+        login = findViewById(R.id.login);
+        signup = findViewById(R.id.signup);
+        name = findViewById(R.id.name);
+        eid = findViewById(R.id.eid);
+        pnumber = findViewById(R.id.pnumber);
+        password = findViewById(R.id.password);
+        username = findViewById(R.id.username);
+        userpassword = findViewById(R.id.userpassword);
+        DB = new userDBhelper(this);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        page = findViewById(R.id.page);
-        login = findViewById(R.id.login);
-
+        getElements();
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //check for correct credentials
-                //if correct going to loading screen
-                Intent secondPage = new Intent(MainActivity.this, loadingScreen.class);
-                startActivity(secondPage);
-                //if not correct show error message
+                boolean userexist  = checkUser();
+                if (userexist) {
+                    Intent secondPage = new Intent(MainActivity.this, loadingScreen.class);
+                    startActivity(secondPage);
+                }
+                else
+                    Toast.makeText(MainActivity.this, "user Not Found", Toast.LENGTH_SHORT).show();
+
             }
         });
+
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nameTXT = name.getText().toString();
+                String eidTXT = eid.getText().toString();
+                String pnumberTXT = pnumber.getText().toString();
+                String passwordTXT = password.getText().toString();
+                Boolean checkInsert = DB.insertUser(nameTXT, eidTXT, passwordTXT, pnumberTXT);
+                if (checkInsert) {
+                    Toast.makeText(MainActivity.this, "User Created !", Toast.LENGTH_SHORT).show();
+                    name.setText(""); eid.setText(""); pnumber.setText(""); password.setText("");
+                } else
+                    Toast.makeText(MainActivity.this, "User not created", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    private boolean checkUser() {
+        String loginName;
+        String loginPassword;
+        loginName = username.getText().toString();
+        loginPassword = userpassword.getText().toString();
+        Cursor res = DB.getdata();
+        if(res.getCount() <= 0)
+            return false;
+        while(res.moveToNext()){
+//            Toast.makeText(this, res.getString(2), Toast.LENGTH_SHORT).show();
+            if (res.getString(1).equals(loginName) && res.getString(2).equals(loginPassword)) {
+                Toast.makeText(this, "Welcome "+ res.getString(0)+" !", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        }
+        return false;
 
     }
 }
